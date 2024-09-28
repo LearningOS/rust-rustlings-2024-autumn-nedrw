6e1953d3364a8dd3ac7d4180c7bede92e51a6cdc
drmin <bjjfcbj@gmail.com>
2024-09-29 01:33:27 +0800

    quiz

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -17,21 +17,15 @@
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T:std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
-        if self.grade <= 2.0 {
-            format!("{} ({}) - achieved a grade of A+",
-            &self.student_name, &self.student_age)
-        } else {
-            format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
-        }
+        format!("{} ({}) - achieved a grade of {}", &self.student_name, &self.student_age, &self.grade)
     }
 }
 
@@ -56,11 +50,10 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let mut report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
-        report_card.grade = 2.0;
         assert_eq!(
             report_card.print(),
             "Gary Plotter (11) - achieved a grade of A+"
